e54990ab48a58e118e00a324c51ac5b43d67926f
Artem Savchenko <savchenkooad@gmail.com>
2023-11-07 09:37:44 +0200

    Added DTOs and Service's interfaces

---
+++ ServiceContracts/CustomValidationAttributes/ValidDateAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ServiceContracts.CustomValidationAttributes
+{
+	public class ValidDateAttribute : ValidationAttribute
+	{
+		private readonly DateTime _minDate;
+
+		public ValidDateAttribute()
+		{
+			_minDate = new DateTime(2000, 1, 1);
+		}
+
+		public override bool IsValid(object? value)
+		{
+			if (value is DateTime date)
+			{
+				return date >= _minDate;
+			}
+			return false;
+		}
+	}
+}
---
+++ ServiceContracts/DTO/BuyOrderRequest.cs
@@ -0,0 +1,16 @@
+﻿using ServiceContracts.CustomValidationAttributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace ServiceContracts.DTO
+{
+	internal class BuyOrderRequest
+	{
+		[Required]
+		public string? StockSymbol { get; set; }
+		[Required]
+		public string? StockName { get; set; }
+		[ValidDate]
+		public DateTime OrderDateTime { get; set; }
+
+	}
+}
