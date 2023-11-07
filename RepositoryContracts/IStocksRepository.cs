1e73d4c1545d507986cfb368e653c9a1da876254
Artem Savchenko <savchenkooad@gmail.com>
2023-11-07 09:21:51 +0200

    Updated RepositoryContracts

--- RepositoryContracts/IStocksRepository.cs
+++ RepositoryContracts/IStocksRepository.cs
@@ -1,12 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Entities;
 
 namespace RepositoryContracts
 {
-	internal interface IStocksRepository
+	public interface IStocksRepository
 	{
+		Task<BuyOrder> CreateBuyOrder(BuyOrder buyOrder);
+
+		Task<SellOrder> CreateSellOrder(SellOrder sellOrder);
+
+		Task<List<BuyOrder>> GetBuyOrders();
+
+		Task<List<SellOrder>> GetSellOrders();
 	}
 }
